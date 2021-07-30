2b037def2556e503153e85fb7c71c7b19eddb613
V <V@MacBook-Pro-de-V.local>
2021-07-30 13:30:31 +0200

    working on songs from desktop JS

--- javascript/playlist.js
+++ javascript/playlist.js
@@ -2,73 +2,78 @@ let listaDeCanciones = [
     {
         "id": 0,
         "img": "./images/songs.png",
-        "nombre": "Chicken Veggi Salad",
+        "nombre": "Song0",
     },
     {
         "id": 1,
         "img": "./images/songs.png",
-        "nombre": "Chicken Veggi Salad",
+        "nombre": "Song1",
     },
     {
         "id": 2,
         "img": "./images/songs.png",
-        "nombre": "Chicken Veggi Salad",
+        "nombre": "Song2",
     },
     {
         "id": 3,
         "img": "./images/songs.png",
-        "nombre": "Chicken Veggi Salad",
+        "nombre": "Song3",
     },
     {
         "id": 4,
         "img": "./images/songs.png",
-        "nombre": "Chicken Veggi Salad",
+        "nombre": "Song4",
     },
     {
         "id": 5,
         "img": "./images/songs.png",
-        "nombre": "Chicken Veggi Salad",
+        "nombre": "Song5",
     },
     {
         "id": 6,
         "img": "./images/songs.png",
-        "nombre": "Chicken Veggi Salad",
+        "nombre": "Song6",
     },
 
     ];
 
     let localizacion = ".canciones";
 
-function imprimir (listaDeCanciones, localizacion){
+function imprimir (listaObjetos, localizacion){
     let mainCanciones = document.querySelector(localizacion);
     let cancion= "";
     listaObjetos.forEach(function (objeto){
         cancion += `
-        <div class="plato-individual">
-            <div class="img-plato">
-                <img class="plato-comida" src= "${objeto.img}" alt="${objeto.nombre}">
-            </div>
-            <div class="plato-contenido">
-                <h2 class="nombre-plato">${objeto.nombre}</h2>
-                <h3 class="precio-plato"><span>${objeto.precio}</span> €</h3>
-                <p class="descripcion-plato">${objeto.descripcion}</p>
-            </div>
-            <div class="nombreYcontador">
-                <div class="contador">
-                    <button class="menos" onclick="quitarPlato(this, ${objeto.id})" >
-                        <h1 class="signo-">-</h1>
-                    </button>
-    
-                    <input class="numeroContador" value=${objeto.cantidad}></input>
-    
-                    <button class="mas" onclick="añadirPlato(this, ${objeto.id})">
-                        <h2 class="signo+">+</h2>
-                    </button>
-                </div>
-            </div>
+        <div class="song0">
+            <img class="img-canciones" src= "${objeto.img}" alt="${objeto.nombre}">
+            <p class="nombre-canciones">${objeto.nombre}</p>
+        </div>
+        <div class="song1">
+            <img class="img-canciones" src= "${objeto.img}" alt="${objeto.nombre}">
+            <p class="nombre-canciones">${objeto.nombre}</p>
+        </div>
+        <div class="song2">
+            <img class="img-canciones" src= "${objeto.img}" alt="${objeto.nombre}">
+            <p class="nombre-canciones">${objeto.nombre}</p>
+        </div>
+        <div class="song3">
+            <img class="img-canciones" src= "${objeto.img}" alt="${objeto.nombre}">
+            <p class="nombre-canciones">${objeto.nombre}</p>
+        </div>
+        <div class="song4">
+            <img class="img-canciones" src= "${objeto.img}" alt="${objeto.nombre}">
+            <p class="nombre-canciones">${objeto.nombre}</p>
+        </div>
+        <div class="song5">
+            <img class="img-canciones" src= "${objeto.img}" alt="${objeto.nombre}">
+            <p class="nombre-canciones">${objeto.nombre}</p>
+        </div>
+        <div class="song6">
+            <img class="img-canciones" src= "${objeto.img}" alt="${objeto.nombre}">
+            <p class="nombre-canciones">${objeto.nombre}</p>
         </div>
         `;
     });
-    mainPlatos.innerHTML = plato;
+    mainCanciones.innerHTML = cancion;
 }
-imprimir(cartaDePlatos, localizacion);
+imprimir(listaDeCanciones, localizacion);
